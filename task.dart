113eb5a0155ef121904a3871af1f574daf203e81
maecrypty-cloud <maecrypty@gmail.com>
2025-07-22 11:51:37 +0530

    Update task.dart

--- task.dart
+++ task.dart
@@ -1,10 +1,48 @@
 // models/task.dart
 
-enum TaskCategory { urgentImportant, importantNotUrgent, urgentNotImportant, notUrgentNotImportant, }
+enum TaskCategory {
+  urgentImportant,
+  importantNotUrgent,
+  urgentNotImportant,
+  notUrgentNotImportant,
+}
 
-class Task { String title; int durationMinutes; bool isCompleted; TaskCategory category; DateTime createdAt;
+class Task {
+  String title;
+  int durationMinutes;
+  bool isCompleted;
+  bool isHabit;
+  bool isMissed;
+  TaskCategory category;
+  DateTime createdAt;
+  DateTime? dueTime;
+  bool repeatsDaily;
 
-Task({ required this.title, required this.durationMinutes, required this.category, this.isCompleted = false, }) : createdAt = DateTime.now();
+  Task({
+    required this.title,
+    required this.durationMinutes,
+    required this.category,
+    this.isCompleted = false,
+    this.isHabit = false,
+    this.isMissed = false,
+    this.repeatsDaily = false,
+    this.dueTime,
+  }) : createdAt = DateTime.now();
 
-int get xp => durationMinutes * 2; }
+  int get xp => durationMinutes * 2;
 
+  void markCompleted() {
+    isCompleted = true;
+    isMissed = false;
+  }
+
+  void markMissed() {
+    if (!isCompleted) {
+      isMissed = true;
+    }
+  }
+
+  bool get isOverdue {
+    return dueTime != null && DateTime.now().isAfter(dueTime!) && !isCompleted;
+  }
+}
