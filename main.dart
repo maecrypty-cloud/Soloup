1218495d9e3478b259d905887239489f9b13a7da
maecrypty-cloud <maecrypty@gmail.com>
2025-07-22 11:57:47 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,8 +1,26 @@
-// main.dart import 'package:flutter/material.dart'; import 'package:solo_up/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+import 'add_task_screen.dart';
 
-void main() { runApp(const SoloUpApp()); }
+void main() {
+  runApp(const SoloUpApp());
+}
 
-class SoloUpApp extends StatelessWidget { const SoloUpApp({super.key});
+class SoloUpApp extends StatelessWidget {
+  const SoloUpApp({super.key});
 
-@override Widget build(BuildContext context) { return MaterialApp( title: 'Solo Up', debugShowCheckedModeBanner: false, theme: ThemeData.dark().copyWith( primaryColor: Colors.deepPurple, scaffoldBackgroundColor: const Color(0xFF121212), colorScheme: ColorScheme.dark( primary: Colors.deepPurple, secondary: Colors.purpleAccent, ), checkboxTheme: CheckboxThemeData( fillColor: MaterialStateProperty.all(Colors.deepPurpleAccent), ), ), home: const HomeScreen(), ); } }
-
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Solo Up',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomeScreen(),
+        '/add': (context) => const AddTaskScreen(),
+      },
+    );
+  }
+}
