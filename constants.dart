d9cebad6e2b313235d205db765586abea0d37e84
maecrypty-cloud <maecrypty@gmail.com>
2025-07-22 11:46:32 +0530

    Create constants.dart

---
+++ constants.dart
@@ -0,0 +1,8 @@
+// lib/utils/constants.dart
+
+import 'package:flutter/material.dart';
+
+class AppColors { static const Color background = Color(0xFF121212); static const Color card = Color(0xFF1E1E1E); static const Color primary = Color(0xFF8AB4F8); static const Color accent = Color(0xFFBB86FC); static const Color error = Color(0xFFCF6679); static const Color success = Color(0xFF03DAC5); static const Color xpColor = Color(0xFFFFC107); }
+
+class AppConstants { static const int dailyTaskRequirement = 5; static const int xpPerTask = 10; static const int xpPenaltyForgive = 5; static const int xpForgiveLimit = 3; static const String appName = 'Solo Up'; }
+
